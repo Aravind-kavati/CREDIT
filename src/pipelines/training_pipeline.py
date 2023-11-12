31f70b56e7074c3629c5404ff77bd9d6b8dfcbf8
Aravind-kavati <aravindkavati83@gmail.com>
2023-11-12 14:59:14 +0530

    start

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -7,3 +7,4 @@ from src.components.data_ingestion import Dataingestion
 if __name__=='__main__':
     obj=Dataingestion()
     train_data_path,test_data_path=obj.initiate_data_ingestion()
+    print(train_data_path,test_data_path)
